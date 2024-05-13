f912e5e293e04ad8f5aa96b2393fcd73ea9669d0
BlueVelocity <josephrodney@hotmail.com>
2024-05-12 20:29:40 -0400

    Add calculator object with tests

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function capitalize(string) {
-  if (typeof string !== "string") throw new Error("Invalid input type");
+  if (typeof string !== "string") throw new Error("Invalid input type, must be string");
+  if (string.length === 0) throw new Error("Invalid Input: Empty string")
   return string[0].toUpperCase() + string.slice(1, string.length);
 }
 
 function reverseString(string) {
-  if (typeof string !== "string") throw new Error("Invalid input type");
+  if (typeof string !== "string") throw new Error("Invalid input type, must be string");
   let reversedString = "";
   for (let i = string.length - 1; i >= 0; i--) {
     reversedString += string[i];
@@ -12,4 +13,34 @@ function reverseString(string) {
   return reversedString;
 }
 
-export { capitalize, reverseString };
+const calculator = {
+  checkForNumber(a, b) {
+    if (typeof a !== "number" || typeof b !== "number") throw new Error("Invalid input type, must be number");
+  },
+  add(a, b) {
+    this.checkForNumber(a, b);
+    return a + b;
+  },
+
+  subtract(a, b) {
+    this.checkForNumber(a, b);
+    return a - b;
+  },
+  
+  multiply(a, b) {
+    this.checkForNumber(a, b);
+    return a * b;
+  },
+
+  divide(a, b) {
+    if (b == 0) throw new Error("Cannot divide by 0");
+    this.checkForNumber(a, b);
+    return a / b;
+  }
+}
+
+function caeserCipher() {
+
+}
+
+export { capitalize, reverseString, calculator, caeserCipher };
--- script.spec.js
+++ script.spec.js
@@ -1,4 +1,4 @@
-import { capitalize, reverseString } from "./script.js";
+import { capitalize, reverseString, calculator } from "./script.js";
 
 //capitalize funtion tests
 test("capitalize() output string is capitalized", () => {
@@ -18,6 +18,10 @@ test("capitalize() input is a string", () => {
   })
 })
 
+test("capitilize() throws error on empty string", () => {
+  expect( () => capitalize("")).toThrow("Invalid Input: Empty string");
+})
+
 //reverseString function tests
 test("reverseString() output string is reversed", () => {
   const testCases = [
@@ -35,3 +39,56 @@ test("reverseString() input is a string", () => {
     expect(() => reverseString(testCase)).toThrow("Invalid input type");
   })
 })
+
+//calculator object tests
+test("calculator add() output adds numbers", () => {
+  const testCases = [
+    {case: [15, 5], result: 20},
+    {case: [-40, 4], result: -36}
+  ];
+  testCases.forEach( (testCase) => {
+    expect(calculator.add(testCase.case[0], testCase.case[1])).toBe(testCase.result);
+  })
+})
+
+test("calculator subtract() output subtracts numbers", () => {
+  const testCases = [
+    {case: [15, 5], result: 10},
+    {case: [-40, 4], result: -44}
+  ];
+  testCases.forEach( (testCase) => {
+    expect(calculator.subtract(testCase.case[0], testCase.case[1])).toBe(testCase.result);
+  })
+})
+
+test("calculator multiply() output multiplies numbers", () => {
+  const testCases = [
+    {case: [15, 5], result: 75},
+    {case: [-40, 4], result: -160}
+  ];
+  testCases.forEach( (testCase) => {
+    expect(calculator.multiply(testCase.case[0], testCase.case[1])).toBe(testCase.result);
+  })
+})
+
+test("calculator divide() output divides numbers", () => {
+  const testCases = [
+    {case: [15, 5], result: 3},
+    {case: [-40, 4], result: -10}
+  ];
+  testCases.forEach( (testCase) => {
+    expect(calculator.divide(testCase.case[0], testCase.case[1])).toBe(testCase.result);
+  })
+})
+
+test("calculator divide() throws error if divide by 0", () => {
+  expect( () => calculator.divide(9, 0)).toThrow("Cannot divide by 0");
+})
+
+test("calculator functions only accept numbers", () => {
+  expect( () => calculator.add("6", "12")).toThrow(Error);
+  expect( () => calculator.subtract("6", "12")).toThrow(Error);
+  expect( () => calculator.multiply("6", "12")).toThrow(Error);
+  expect( () => calculator.divide("6", "12")).toThrow(Error);
+})
+
